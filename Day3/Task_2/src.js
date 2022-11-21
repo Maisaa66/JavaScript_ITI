0d295b8075dd923cbbf3738b0b8c3c979a5174a6
Maisaa Hamza <maisaa.hamza66@gmail.com>
2022-11-21 19:47:25 +0200

    Day3-Task2

---
+++ Day3/Task_2/src.js
@@ -0,0 +1,16 @@
+// get the area of a circle
+var radius = prompt("Please enter radius");
+
+alert("The ares of circle is " + Math.PI * radius * radius);
+
+// calculate the square root
+
+var sqroot =  prompt("Please enter the value you want its square root");
+
+alert("Square root of " + sqroot + " is "+ Math.sqrt(sqroot));
+
+// get cosine of angle
+
+var angle = prompt("Please enter the angle you want to get its cosine");
+document.write("cos " + angle+"&#176;" + " is " + Math.cos(parseInt(angle)));
+

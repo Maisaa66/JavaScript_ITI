7e96e950d84e6daeb83165f2e054baff70e00cfa
Maisaa Hamza <maisaa.hamza66@gmail.com>
2022-11-24 18:31:06 +0200

    update js and css
    in js i made a function to set border on image when the radio button is checked

---
+++ Day6/Card Generator/main.js
@@ -0,0 +1,18 @@
+let images=document.images
+
+function setBorder(id){
+
+for(i=0;i<images.length;i++){
+    if(document.images[i].id==id){
+        document.images[i].style.border="5px solid maroon";
+    }
+    else{
+        document.images[i].style.border="";
+
+    }
+
+}
+
+
+
+}

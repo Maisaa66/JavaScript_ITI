7913f679db07c01f71dc4c671b09bc8f46c4e07b
Maisaa Hamza <maisaa.hamza66@gmail.com>
2022-11-26 20:30:41 +0200

    Add some comments

--- Day6/DOM Tasks/DOM/main.js
+++ Day6/DOM Tasks/DOM/main.js
@@ -1,23 +1,28 @@
-//deep copy for the image and then append it at the end of the page
 
 function change(){
 
-
+//get the div of the image as it is the second child of the first childern
 let imageContainer = document.body.firstElementChild.children.item(1)
 
+//get the image
 let image = imageContainer.firstElementChild
 
+//deep copy for the image and then append it at the end of the page
 let copiedImage=image.cloneNode();
 
+//style the new image
 image.style.float='right';
 imageContainer.style.width='100%';
 imageContainer.style.height='229px';
 
+//get the menu div and the menu it self
 let listContainer = document.body.firstElementChild.children.item(2)
 let list = document.body.firstElementChild.children.item(2).firstElementChild
+//style the original menu
 list.style.listStyleType ='circle';
 listContainer.style.display='inline-block'
 
+//append the new image copied
 document.body.append(copiedImage);
 
 }
